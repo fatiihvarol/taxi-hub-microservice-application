13f537ddc07d52c892192546b05c943cc01beb22
fatiihvarol <fatihvrl38@gmail.com>
2025-12-02 22:46:42 +0300

    response boddy and error messages added to elastic

--- api-gateway/middleware/logger.go
+++ api-gateway/middleware/logger.go
@@ -11,43 +11,53 @@ import (
 )
 
 type LogModel struct {
-	Method     string `json:"method"`
-	Path       string `json:"path"`
-	Status     int    `json:"status"`
-	IP         string `json:"ip"`
-	UserAgent  string `json:"user_agent"`
-	DurationMs int64  `json:"duration_ms"`
-	Error      string `json:"error,omitempty"`
-	Timestamp  string `json:"timestamp"`
+	Method      string `json:"method"`
+	Path        string `json:"path"`
+	Status      int    `json:"status"`
+	IP          string `json:"ip"`
+	UserAgent   string `json:"user_agent"`
+	DurationMs  int64  `json:"duration_ms"`
+	RequestBody string `json:"request_body,omitempty"`
+	Response    string `json:"response_body,omitempty"`
+	Error       string `json:"error,omitempty"`
+	Timestamp   string `json:"timestamp"`
 }
 
-func GlobalLogger() fiber.Handler {
-    return func(c *fiber.Ctx) error {
-        start := time.Now()
+func GlobalLogger(c *fiber.Ctx) error {
+	start := time.Now()
 
-        err := c.Next()
-        duration := time.Since(start).Milliseconds()
+	// Request body yakala
+	reqBody := string(c.Body())
 
-        logItem := LogModel{
-            Method:     c.Method(),
-            Path:       c.Path(),
-            Status:     c.Response().StatusCode(),
-            IP:         c.IP(),
-            UserAgent:  string(c.Request().Header.UserAgent()),
-            DurationMs: duration,
-            Timestamp:  time.Now().Format(time.RFC3339),
-        }
+	// Handler çalıştır
+	err := c.Next()
+	duration := time.Since(start).Milliseconds()
 
-        if err != nil {
-            logItem.Error = err.Error()
-        }
+	// Response body yakala
+	resBody := string(c.Response().Body())
 
-        go sendToElastic(logItem)
+	logItem := LogModel{
+		Method:      c.Method(),
+		Path:        c.Path(),
+		Status:      c.Response().StatusCode(),
+		IP:          c.IP(),
+		UserAgent:   string(c.Request().Header.UserAgent()),
+		DurationMs:  duration,
+		RequestBody: reqBody,
+		Response:    resBody,
+		Timestamp:   time.Now().Format(time.RFC3339),
+	}
 
-        return err
-    }
+	// Hata varsa ekle
+	if err != nil {
+		logItem.Error = err.Error()
+	}
+
+	// Elastic'e async gönder
+	go sendToElastic(logItem)
+
+	return err
 }
-
 
 func sendToElastic(item LogModel) {
 	data, _ := json.Marshal(item)
@@ -56,11 +66,9 @@ func sendToElastic(item LogModel) {
 		elastic.ElasticIndex,
 		bytes.NewReader(data),
 	)
-
 	if err != nil {
-		 log.Printf("[Elastic] insert error: %v", err)
-		 return
+		log.Printf("[Elastic] insert error: %v", err)
+		return
 	}
-
 	defer res.Body.Close()
 }
